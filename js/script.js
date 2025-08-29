576d34d6e360b19950fcb0b703fc2863f6c8c3b4
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-29 11:04:57 +0600

    heart button js done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // script part
+// hart button click
+const hartButtons = document.querySelectorAll('#btn-heart');
+for (let i = 0; i < hartButtons.length; i++) {
+  hartButtons[i].addEventListener('click', function() {
+    let hartCounter = document.getElementById('heart-count');
+    hartCounter.innerText = parseInt(hartCounter.innerText) + 1;
+  });
+}
